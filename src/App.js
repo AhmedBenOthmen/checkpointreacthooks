b7cb18c48c3b8ba7572230dc33a6103f2b48a751
Ahmed_Ben_Othmen <Benothmenahmed1989@gmail.com>
2023-10-26 19:29:23 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import './App.css';
-import MovieList from './components/MovieList/MovieList.js';
-import Filter from './components/Filter/Filter.js';
-import AddMovie from './components/AddMovie/AddMovie.js';
-import useApp from './useApp.js';
+import "./App.css";
+import MovieList from "./components/MovieList/MovieList.js";
+import Filter from "./components/Filter/Filter.js";
+import AddMovie from "./components/AddMovie/AddMovie.js";
+import useApp from "./useApp.js";
 
 function App() {
- 
-  const { addMovie , filterByRating , filterByTitle , filteredMovies  } = useApp();
+  const {
+    addMovie,
+    filterByRating,
+    filterByTitle,
+    filteredMovies,
+    contentToRender,
+  } = useApp();
 
   return (
     <div className="App">
       <h1>Movie Library</h1>
+
       {/* Filter component */}
       <Filter filterTitle={filterByTitle} filterRating={filterByRating} />
+
       {/* MovieList component */}
-      <MovieList movies={filteredMovies} />
+      {contentToRender ? (
+        <MovieList movies={filteredMovies} />
+      ) : (
+        <h1>No Movies Found</h1>
+      )}
+
       {/* AddMovie Component */}
       <AddMovie AddMovie={addMovie} />
     </div>
--- src/useApp.js
+++ src/useApp.js
@@ -1,36 +1,47 @@
-import  {useState} from 'react';
-import MoviesData from './components/MoviesData/Moviesdata.js';
+import { useState } from "react";
+import MoviesData from "./components/MoviesData/Moviesdata.js";
 
-function useApp () {
+function useApp() {
+  // Use the imported MoviesData
+  const [movies, setMovies] = useState(MoviesData);
 
- // Use the imported MoviesData
- const [movies, setMovies] = useState(MoviesData); 
+  //useState Hook for the filtered list of movies
+  const [filteredMovies, setFilteredMovies] = useState(movies);
 
- //useState Hook for the filtered list of movies
- const [filteredMovies, setFilteredMovies] = useState(movies); 
+  const contentToRender = filteredMovies.length > 0;
 
- //filter by title function
- const filterByTitle = (title) => {
-   const filtered = movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
-   setFilteredMovies(filtered);
- }
+  //filter by title function
+  const filterByTitle = (title) => {
+    const filtered = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(title.toLowerCase())
+    );
+    setFilteredMovies(filtered);
+  };
 
- //filter by rating function
- const filterByRating = (rating) => {
-   const filtered = movies.filter(movie => movie.rating >= parseFloat(rating));
-   setFilteredMovies(filtered);
- }
+  //filter by rating function
+  const filterByRating = (rating) => {
+    const filtered = movies.filter(
+      (movie) => movie.rating >= parseFloat(rating)
+    );
+    setFilteredMovies(filtered);
+  };
 
- const addMovie = (newMovie) => {
-   // Create a copy of the current movies array and add the new movie
-   const updatedMovies = [...movies, newMovie];
-   setMovies(updatedMovies);
-   // Update the filteredMovies
-   setFilteredMovies(updatedMovies);
- };
+  const addMovie = (newMovie) => {
+    // Create a copy of the current movies array and add the new movie
+    const updatedMovies = [...movies, newMovie];
+    setMovies(updatedMovies);
+    // Update the filteredMovies
+    setFilteredMovies(updatedMovies);
+  };
 
- return { addMovie , filterByRating , filterByTitle , filteredMovies ,movies }
-
+  return {
+    addMovie,
+    filterByRating,
+    filterByTitle,
+    filteredMovies,
+    movies,
+    contentToRender,
+  };
 }
 
-export default useApp
+export default useApp;
