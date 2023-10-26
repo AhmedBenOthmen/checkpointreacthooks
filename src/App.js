ca791813ce2bb5656f9a93a55519a477c648c45c
Ahmed_Ben_Othmen <Benothmenahmed1989@gmail.com>
2023-10-26 18:46:42 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
-import React, {useState} from 'react';
 import MovieList from './components/MovieList/MovieList.js';
 import Filter from './components/Filter/Filter.js';
-import MoviesData from './components/MoviesData/Moviesdata.js';
 import AddMovie from './components/AddMovie/AddMovie.js';
+import useApp from './useApp.js';
 
 function App() {
-  // Use the imported MoviesData
-  const [movies, setMovies] = useState(MoviesData); 
-
-  //useState Hook for the filtered list of movies
-  const [filteredMovies, setFilteredMovies] = useState(movies); 
-
-  //filter by title function
-  const filterByTitle = (title) => {
-    const filtered = movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
-    setFilteredMovies(filtered);
-  }
-
-  //filter by rating function
-  const filterByRating = (rating) => {
-    const filtered = movies.filter(movie => movie.rating >= parseFloat(rating));
-    setFilteredMovies(filtered);
-  }
-
-  const addMovie = (newMovie) => {
-    // Create a copy of the current movies array and add the new movie
-    const updatedMovies = [...movies, newMovie];
-    setMovies(updatedMovies);
-    // Update the filteredMovies
-    setFilteredMovies(updatedMovies);
-  };
+ 
+  const { addMovie , filterByRating , filterByTitle , filteredMovies  } = useApp();
 
   return (
     <div className="App">
